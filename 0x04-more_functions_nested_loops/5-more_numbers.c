afa863eaef7faa7ce9cbf003fa5b921936f71017
Edward Brew <itprofessional8931@gmail.com>
2022-04-14 22:07:41 +0000

    C-program More function task 3 - 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14.
+ *
+*/
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m > 9)
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+		}
+			_putchar('\n');
+	}
+}
