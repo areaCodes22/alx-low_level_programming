a1317ff3072aaa523f485b34d5e87c75ce09ea02
Edward <areafather24@gmail.com>
2022-05-06 00:45:11 +0000

    My commit on Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * str_concat - concatenates two strings.
@@ -11,33 +11,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *strout;
-	unsigned int i, j, k, limit;
+
+	int i, len1, len2;
+	char *cc;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++);
+		len1 = _strlen(s1);
+		len2 = _strlen(s2);
 
-	for (j = 0; s2[j] != '\0'; j++);
-
-	strout = malloc(sizeof(char) * (i + j + 1));
-
-	if (strout == NULL)
-	{
-		free(strout);
+	cc = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (cc == NULL)
 		return (NULL);
-	}
 
-	for (k = 0; k < i; k++)
-		strout[k] = s1[k];
+	for (i = 0; i < len1; i++)
+		cc[i] = s1[i];
+	for (; i < len1 + len2; i++)
+		cc[i] = s2[i - len1];
+	cc[len1 + len2] = '\0';
 
-	limit = j;
-	for (j = 0; j <= limit; k++, j++)
-		strout[k] = s2[j];
+	return (cc);
 
-	return (strout);
 }
